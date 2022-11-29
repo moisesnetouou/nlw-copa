9959f9920a7ffec348c5dbe4ad971136bf414772
Moisés Neto <moisesnetored@gmail.com>
2022-11-28 23:21:34 -0400

    feat: custom font roboto added and loading font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from "native-base"
-
+import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto"
+import {Loading} from "./src/components/Loading"
 import { THEME } from "./src/styles/theme"
-
+import {SignIn} from "./src/screens/SignIn"
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular, Roboto_500Medium, Roboto_700Bold
+  })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>Hello React Native</Text>
-        <StatusBar style="auto" />
-      </Center>
+      {fontsLoaded
+      ? 
+      (
+        <>
+          <SignIn />
+          <StatusBar style="auto" />
+        </>
+      )
+      : 
+      <Loading />
+    }
     </NativeBaseProvider>
   );
 }

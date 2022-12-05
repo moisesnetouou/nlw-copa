78b9e7274a7acf725aa76bed52e0db6092c95570
Moisés Neto <moisesnetored@gmail.com>
2022-12-04 22:51:56 -0400

    feat: create pool client side

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,6 +5,7 @@ import logoImg from "../assets/images/logo.svg"
 import usersAvatarExampleImg from "../assets/images/users-avatar-example.png"
 import iconCheckImg from "../assets/images/icon-check.svg"
 import { api } from "../lib/axios";
+import { FormEvent, useState } from "react";
 
 interface HomeProps {
   poolCount: number;
@@ -13,6 +14,27 @@ interface HomeProps {
 }
 
 export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState("")
+
+  async function createPool(event: FormEvent){
+    event.preventDefault()
+
+    try {
+      const response = await api.post("/pools", {
+        title: poolTitle
+      })
+
+      const {code} = response.data;
+
+      await navigator.clipboard.writeText(code)
+
+      setPoolTitle("")
+      alert("Bolão criado com sucesso, o código foi copiado para areá de transferência")
+    } catch (error) {
+      alert("Falha ao criar o bolão, tente novamente")
+    }
+  }
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
       <main>
@@ -29,12 +51,14 @@ export default function Home(props: HomeProps) {
           </strong>
         </div>
 
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
           <input
-            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100"
             type="text" 
             required 
-            placeholder="Qual nome do seu bolão?" 
+            placeholder="Qual nome do seu bolão?"
+            value={poolTitle} 
+            onChange={(event) => setPoolTitle(event.target.value)}
           />
           <button 
             className="bg-yellow-500 px-6 py-4 rounded font-bold uppercase text-gray-900 text-sm hover:bg-yellow-700"

e27534333863650c5230d3e8c8da72699165bf74
Moisés Neto <moisesnetored@gmail.com>
2022-12-04 22:18:07 -0400

    feat: home page created

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,16 +1,81 @@
+import Image from "next/image";
+
+import appPreviewImg from "../assets/images/app-nlw-copa-preview.png"
+import logoImg from "../assets/images/logo.svg"
+import usersAvatarExampleImg from "../assets/images/users-avatar-example.png"
+import iconCheckImg from "../assets/images/icon-check.svg"
+
 interface HomeProps {
   count: number;
 }
 
-export default function Home(props: HomeProps) {
+export default function Home() {
   return (
-    <div>
-      <h1>Contagem: {props.count}</h1>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
+      <main>
+        <Image src={logoImg} alt="NLW Copa" />
+        <h1 className="mt-14 text-white text-5xl font-bold leading-tight">
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className="mt-10 flex items-center gap-2">
+          <Image src={usersAvatarExampleImg} alt="" />
+
+          <strong className="text-gray-100 text-xl">
+            <span className="text-ignite-500">+12.592</span> pessoas já estão usando
+          </strong>
+        </div>
+
+        <form className="mt-10 flex gap-2">
+          <input
+            className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm"
+            type="text" 
+            required 
+            placeholder="Qual nome do seu bolão?" 
+          />
+          <button 
+            className="bg-yellow-500 px-6 py-4 rounded font-bold uppercase text-gray-900 text-sm hover:bg-yellow-700"
+            type="submit"
+          >
+              Criar meu bolão
+          </button>
+        </form>
+
+        <p className="mt-4 text-sm text-gray-300 leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar pessoas 🚀 
+        </p>
+
+        <div className="mt-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100">
+          <div className="flex items-center gap-6">
+            <Image src={iconCheckImg} alt="" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+
+          <div className="w-px h-14 bg-gray-600" />
+
+          <div className="flex items-center gap-6">
+            <Image src={iconCheckImg} alt="" />
+            <div className="flex flex-col">
+              <span className="font-bold text-2xl">+2.034</span>
+              <span>Palpites enviados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <Image 
+        src={appPreviewImg} 
+        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW copa" 
+        quality={100}
+      />
     </div>
   )
 }
 
-export const getServerSideProps = async ()=> {
+/* export const getServerSideProps = async ()=> {
   const response = await fetch("http://localhost:3333/pools/count")
   const data = await response.json()
   console.log(data)
@@ -21,3 +86,4 @@ export const getServerSideProps = async ()=> {
     }
   }
 }
+ */
